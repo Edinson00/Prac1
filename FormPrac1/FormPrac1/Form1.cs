9aad325c93d005a87c8a5bbd42a8c9c5a4d10c58
Edinson Bornachera <Edinson693693@gmail.com>
2021-09-09 16:56:25 -0500

    Completo
    ya se pueden hacer las consultas de la base de datos

--- FormPrac1/FormPrac1/Form1.cs
+++ FormPrac1/FormPrac1/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,45 @@ namespace FormPrac1
 {
     public partial class Form1 : Form
     {
+        public SqlConnection con = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DBPrac1;Integrated Security=True");
+        SqlCommand cmd;
+        SqlDataReader dr;
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            con.Open();
+            MessageBox.Show("Conexion Creada Con Exito!");
+            con.Close(); 
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+
+            string query = "Select  * from Alumno where Edad>15";
+            cmd = new SqlCommand(query, con);
+
+            //set the SqlDataAdapter object
+            SqlDataAdapter dAdapter = new SqlDataAdapter(cmd);
+
+            //define dataset
+            DataSet ds = new DataSet();
+
+            //fill dataset with query results
+            dAdapter.Fill(ds);
+
+            dataGridView1.DataSource = ds.Tables[0];
+
+            con.Close();
         }
     }
 }
