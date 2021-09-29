06d51fd07513ed456de47c1b7cdccf83ff4dc895
Edinson Bornachera <Edinson693693@gmail.com>
2021-09-28 20:22:22 -0500

    Pruebas

--- FormPrac1/FormPrac1/Form1.cs
+++ FormPrac1/FormPrac1/Form1.cs
@@ -38,7 +38,7 @@ namespace FormPrac1
         {
             con.Open();
 
-            string query = "Select  * from Alumno where Edad>15";
+            string query = "Select  * from Alumno where Id=1";
             cmd = new SqlCommand(query, con);
 
             //set the SqlDataAdapter object
